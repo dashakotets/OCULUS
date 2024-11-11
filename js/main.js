711e49ab01c144ca1e6b72c5fd8b5dd784885c0a
dashakotets <dashacot1@gmail.com>
2024-11-11 15:51:17 +0000

    adaptive done

--- js/main.js
+++ js/main.js
@@ -24,3 +24,10 @@ function open(event) {
 
 
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-active');
+})
